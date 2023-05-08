9cf8ffb31624aed1d9003836abdd00597f225a65
Tomek Leszczynski <hiszpania2018@gmail.com>
2023-05-08 11:21:53 +0200

    All functions works. Enable keyboard operations.Project completed.

--- script.js
+++ script.js
@@ -1,67 +1,65 @@
-const numberBtns = document.querySelectorAll('[data-digit]')
 const operators = document.querySelectorAll('[data-operator]')
-const mainDisplay = document.querySelector('.main-line')
-const resetBtn = document.querySelector('[data-reset]')
+const display = document.querySelector('.main-line')
+const numberButtons = document.querySelectorAll('[data-digit]')
+const commaBtn = document.querySelector('[data-point]')
 const equalBtn = document.querySelector('[data-equal]')
 const backspaceBtn = document.querySelector('[data-backspace]')
-const commaBtn = document.querySelector('[data-point]')
+const resetBtn = document.querySelector('[data-reset]')
 
 let operandA
 let operandB
 let operator
 let result
-let clearDisplay = false
+let displayToClear = false
 
-function clearMainDisplay() {
-	mainDisplay.textContent = ''
-	clearDisplay = false
+function clearDisplay() {
+	display.textContent = ''
+	displayToClear = false
 }
 function reset() {
-	mainDisplay.textContent = ''
+	display.textContent = ''
 	operandA = ''
 	operandB = ''
 	operator = ''
 	result = ''
-	clearDisplay = false
+	displayToClear = false
 }
 function fixInputData() {
-	mainDisplay.textContent = mainDisplay.textContent.toString().slice(0, -1)
+	display.textContent = display.textContent.toString().slice(0, -1)
 }
-function appendPoint() {
-	if (clearDisplay) clearMainDisplay()
-	if (mainDisplay.textContent.includes('.')) return
-	else if (mainDisplay.textContent === '') mainDisplay.textContent = '0'
-	mainDisplay.textContent += '.'
-}
-function evaluate() {
-	if (result || clearDisplay) return
-	operandB = mainDisplay.textContent
+function checkDivideByZero() {
 	if (operator === '/' && operandB == 0) {
 		alert("Can't divide by 0!")
 		reset()
 		return
 	}
-	clearMainDisplay()
-	doTheMath(operator, Number(operandA), Number(operandB))
-	mainDisplay.textContent = Math.round(result * 1000) / 1000
-	clearDisplay = true
 }
-function appendNums(number) {
-	if (clearDisplay || mainDisplay.textContent === '0') clearMainDisplay()
-	mainDisplay.textContent += number
+function appendPoint() {
+	if (displayToClear) clearDisplay()
+	if (display.textContent.includes('.')) return
+	if (display.textContent === '') display.textContent = '0'
+	display.textContent += '.'
+}
+function appendNumbers(number) {
+	if (displayToClear || display.textContent === '0') clearDisplay()
+	display.textContent += number
 }
 function setOperation(operatorKey) {
-	if (result) {
-		operandA = result
-	} else if (operator) {
-		evaluate()
-	}
+	if (result) operandA = result
+	if (operator) evaluate()
 	result = ''
-	operandA = mainDisplay.textContent
+	operandA = display.textContent
 	operator = operatorKey
-	clearDisplay = true
+	displayToClear = true
 }
-function doTheMath(operator, a, b) {
+function evaluate() {
+	if (result || displayToClear) return
+	operandB = display.textContent
+	checkDivideByZero()
+	doTheMath(Number(operandA), Number(operandB))
+}
+function doTheMath(a, b) {
+	clearDisplay()
 	switch (operator) {
 		case '+':
 			result = a + b
@@ -78,12 +76,22 @@ function doTheMath(operator, a, b) {
 		default:
 			return
 	}
+	display.textContent = Math.round(result * 1000) / 1000
+	displayToClear = true
 }
 
-backspaceBtn.addEventListener('click', fixInputData)
-equalBtn.addEventListener('click', evaluate)
-resetBtn.addEventListener('click', reset)
-numberBtns.forEach(num => num.addEventListener('click', () => appendNums(num.textContent)))
+function handleKeyboard(e) {
+	if (e.key >= 0 && e.key <= 9) appendNumbers(e.key)
+	if (e.key == '.') appendPoint()
+	if (e.key == '+' || e.key == '-' || e.key === '*' || e.key == '/') setOperation(e.key)
+	if (e.key == '=' || e.key == 'Enter') evaluate()
+	if (e.key == 'Escape' || e.key == 'c') reset()
+}
+
+numberButtons.forEach(num => num.addEventListener('click', () => appendNumbers(num.textContent)))
 operators.forEach(operator => operator.addEventListener('click', () => setOperation(operator.textContent)))
+equalBtn.addEventListener('click', evaluate)
+backspaceBtn.addEventListener('click', fixInputData)
+resetBtn.addEventListener('click', reset)
 commaBtn.addEventListener('click', appendPoint)
-// window.addEventListener('keydown', handleKeyboard)
+window.addEventListener('keydown', handleKeyboard)
